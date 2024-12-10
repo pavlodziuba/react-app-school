f839bd42628264dd1fe6f3b0eef6e7590cd3b368
Pavlo Dziuba <pavlo.dziuba@student.tuke.sk>
2024-12-10 16:12:00 +0100

    edit home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,11 +1,38 @@
 import React from "react";
 import "./HomePage.css";
 import { Link } from "react-router-dom";
+import FullWidthText from "../../components/FullWidthText";
+import img1 from "../../photos/2245_img-3_3_2_b.jpg";
+import img2 from "../../photos/2265_img-3_1_1_b.jpg";
+import img3 from "../../photos/2166_img-3_5_2_b.jpg";
+import img4 from "../../photos/2260_img-3_3_2_b.jpg";
+import Classes from "../ParentsPage/Classes";
+import LatestNews from "../NewsPage/LatestNews";
+
 const HomePage = () => {
   return (
     <div className="content">
-      <div className="home">Domov</div>
-      <Link to="/admin">Admin</Link>
+      <FullWidthText id="oznamy" text="Najnovšie správy" />
+      <LatestNews />
+      <FullWidthText id="fotogaleria" text="Fotogaléria" />
+      <div className="photogalery">
+        <div className="photogalery_photo">
+          <img src={img1} alt="news-img"></img>
+        </div>
+        <div className="photogalery_photo">
+          <img src={img2} alt="news-img"></img>
+        </div>
+      </div>
+      <div className="photogalery">
+        <div className="photogalery_photo">
+          <img src={img3} alt="news-img"></img>
+        </div>
+        <div className="photogalery_photo">
+          <img src={img4} alt="news-img"></img>
+        </div>
+      </div>
+      <FullWidthText id="triedy" text="TRIEDY" />
+      <Classes />
     </div>
   );
 };
